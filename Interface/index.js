2739883f3f8e0127e347e28be5da4d112298b2e5
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2018-12-23 15:01:08 -0500

    started work on telnet client

--- Interface/index.js
+++ Interface/index.js
@@ -1,1 +1,20 @@
+const telnet = require("telnet-client")
+
+const run = async function(){
+	let connection = new telnet()
+
+	let params = {
+		host: '10.0.2.15',
+		port: '1234',
+		shellPrompt: '',
+		timeout: 1500
+	}
+
+	await connection.connect(params)
+
+	let res = await connection.exec('uptime')
+
+	console.log(res)
+}
+
 console.log("test")
